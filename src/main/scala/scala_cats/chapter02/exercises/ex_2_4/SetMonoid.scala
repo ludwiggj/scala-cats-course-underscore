2b8af87b944c2386b4551f3354efd6a610fb1a6f
Graeme Ludwig <graeme.ludwig@itv.com>
2021-07-31 22:19:39 +0100

    Fixed implicit derivation errors

--- src/main/scala/scala_cats/chapter02/exercises/ex_2_4/SetMonoid.scala
+++ src/main/scala/scala_cats/chapter02/exercises/ex_2_4/SetMonoid.scala
@@ -2,14 +2,17 @@ package scala_cats.chapter02.exercises.ex_2_4
 
 import scala_cats.chapter02.Monoid
 
+//noinspection MutatorLikeMethodIsParameterless
 object SetMonoid {
-  implicit def setUnionMonoid[A](): Monoid[Set[A]] = new Monoid[Set[A]] {
+  // NOTE: Adding empty brackets for these methods fixes code inspection warnings BUT
+  //       prevents automatic implicit derivation
+  implicit def setUnionMonoid[A]: Monoid[Set[A]] = new Monoid[Set[A]] {
     override def empty: Set[A] = Set.empty
 
     override def combine(x: Set[A], y: Set[A]): Set[A] = x union y
   }
 
-  implicit def setAddMonoid[A](): Monoid[Set[A]] = new Monoid[Set[A]] {
+  implicit def setAddMonoid[A]: Monoid[Set[A]] = new Monoid[Set[A]] {
     override def empty: Set[A] = Set.empty
 
     override def combine(x: Set[A], y: Set[A]): Set[A] = x ++ y
@@ -17,7 +20,7 @@ object SetMonoid {
 
   // intersect - not a monoid, no identity element
 
-  implicit def setSymmetricDifferenceMonoid[A](): Monoid[Set[A]] = new Monoid[Set[A]] {
+  implicit def setSymmetricDifferenceMonoid[A]: Monoid[Set[A]] = new Monoid[Set[A]] {
     override def empty: Set[A] = Set.empty
 
     override def combine(x: Set[A], y: Set[A]): Set[A] = (x diff y) union (y diff x)
--- src/main/scala/scala_cats/chapter02/exercises/ex_2_4/SetSemigroup.scala
+++ src/main/scala/scala_cats/chapter02/exercises/ex_2_4/SetSemigroup.scala
@@ -2,15 +2,18 @@ package scala_cats.chapter02.exercises.ex_2_4
 
 import scala_cats.chapter02.Semigroup
 
+//noinspection MutatorLikeMethodIsParameterless
 object SetSemigroup {
-  implicit def setUnionSemigroup[A](): Semigroup[Set[A]] = (x: Set[A], y: Set[A]) => x union y
+  // NOTE: Adding empty brackets for these methods fixes code inspection warnings BUT
+  //       prevents automatic implicit derivation
+  implicit def setUnionSemigroup[A]: Semigroup[Set[A]] = (x: Set[A], y: Set[A]) => x union y
 
-  implicit def setAddSemigroup[A](): Semigroup[Set[A]] = (x: Set[A], y: Set[A]) => x ++ y
+  implicit def setAddSemigroup[A]: Semigroup[Set[A]] = (x: Set[A], y: Set[A]) => x ++ y
 
-  implicit def setIntersectSemigroup[A](): Semigroup[Set[A]] = (x: Set[A], y: Set[A]) => x intersect y
+  implicit def setIntersectSemigroup[A]: Semigroup[Set[A]] = (x: Set[A], y: Set[A]) => x intersect y
 
-  implicit def setDifferenceNotASemigroup[A](): Semigroup[Set[A]] = (x: Set[A], y: Set[A]) => x diff y
+  implicit def setDifferenceNotASemigroup[A]: Semigroup[Set[A]] = (x: Set[A], y: Set[A]) => x diff y
 
-  implicit def setSymmetricDifferenceSemigroup[A](): Semigroup[Set[A]] =
+  implicit def setSymmetricDifferenceSemigroup[A]: Semigroup[Set[A]] =
     (x: Set[A], y: Set[A]) => (x diff y) union (y diff x)
 }
